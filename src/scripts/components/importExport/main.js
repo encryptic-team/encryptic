760e9c5bfb7cdf9bb84d363e310d4052ac17850b
Brad Arnett <brad.arnett@gmail.com>
2019-03-12 01:58:10 -0500

    removed login migration query

--- src/scripts/components/importExport/main.js
+++ src/scripts/components/importExport/main.js
@@ -16,10 +16,12 @@ function initialize() {
         export         : (...args) => new Export(...args).init(),
     });
 
+    /* Disable login migration check to be in line with issue #58
     Radio.request('utils/Initializer', 'add', {
         name     : 'App:last',
         callback : () => new Migrate().init(),
     });
+    */
 }
 
 Radio.once('App', 'init', initialize);
